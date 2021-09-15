aa495e8affdfed8bd9f04628b0e60e2b4d83c8a7
9958221649 <41771986+9958221649@users.noreply.github.com>
2021-09-15 21:06:15 +0530

    Initial Commit

---
+++ MachineTestGithub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineTestGithub
+//
+//  Created by Mamsys on 15/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
